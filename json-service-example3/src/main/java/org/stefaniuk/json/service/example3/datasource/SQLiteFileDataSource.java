c55ecfb2e46bf5fcfc0c1290a5b48ec4db8c2dfa
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2012-09-04 09:17:19 +0100

    sqlite datasource

---
+++ json-service-example3/src/main/java/org/stefaniuk/json/service/example3/datasource/SQLiteFileDataSource.java
@@ -0,0 +1,22 @@
+package org.stefaniuk.json.service.example3.datasource;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+public class SQLiteFileDataSource extends SingleConnectionDataSource {
+
+    //@Autowired
+    //private HttpSession session;
+
+    //<property name="driverClassName" value="org.sqlite.JDBC" />
+    //<property name="url" value="jdbc:sqlite:/WEB-INF/classes/chinook.sqlite" />
+
+    public SQLiteFileDataSource() {
+
+        System.out.println("-----------------------------------");
+        //System.out.println(session.toString());
+    }
+
+}

5a370999a80acda1cefb54fa004eca5e710df4c1
Daniel Stefaniuk <daniel.stefaniuk@gmail.com>
2012-09-17 17:24:24 +0100

    add test

---
+++ json-service/src/test/java/org/stefaniuk/json/service/JsonServiceRegistryTest.java
@@ -0,0 +1,14 @@
+package org.stefaniuk.json.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JsonServiceRegistryTest {
+
+    @Test
+    public void testSimple() {
+
+        Assert.assertTrue(true);
+    }
+
+}
